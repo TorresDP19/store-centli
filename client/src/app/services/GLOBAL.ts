1cbb98853694e6217ef6a6cb162c9e1696366a2a
TorresDP19 <torresdaniel19dp@gmail.com>
2024-11-28 17:20:39 -0600

    Update cors and change env

--- client/src/app/services/GLOBAL.ts
+++ client/src/app/services/GLOBAL.ts
@@ -1,5 +1,6 @@
 export var GLOBAL = {
-    url: 'http://localhost:3977'
+    //url: 'http://localhost:3977'
+    url: 'https://store-centli-4a739e6d5a7a.herokuapp.com'
     // https://foodday20.herokuapp.com
     // http://localhost:3977
     // http://www.dominio-prueba.xyz
